a36980a82c1bc6e5499bca5edbf15c671b16d9fc
Akaanksh Raj <18450543+goknsh@users.noreply.github.com>
2025-05-04 23:48:47 -0500

    fix(ci): try to get ccache working (#365)

--- apps/client/app/(tabs)/(home)/index.tsx
+++ apps/client/app/(tabs)/(home)/index.tsx
@@ -3,7 +3,7 @@ import { Stack } from 'expo-router';
 import { FloatingPlayerDodgingLayout } from '~/components/floating-player';
 import { Text } from '~/components/ui/text';
 
-import Player, { useAudioPlayerStatus } from '~/modules/voel-audio';
+import { useAudioPlayerStatus } from '~/modules/voel-audio';
 
 export default function HomeScreen() {
   const playerStatus = useAudioPlayerStatus();
@@ -12,10 +12,7 @@ export default function HomeScreen() {
     <>
       <Stack.Screen options={{ title: 'Home' }} />
       <FloatingPlayerDodgingLayout>
-        <Text>Player Status:</Text>
         <Text>{JSON.stringify(playerStatus, null, 2)}</Text>
-        <Text>Player Queue:</Text>
-        <Text>{JSON.stringify(Player.getCurrentQueue(), null, 2)}</Text>
       </FloatingPlayerDodgingLayout>
     </>
   );
