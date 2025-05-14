8a186e7ff762eb6540907227b98cd5e2c15b68b9
Akaanksh Raj <18450543+goknsh@users.noreply.github.com>
2025-05-14 18:35:41 -0500

    fix(client): handle multiple audiobook files, and chapters spanning multiple files

--- apps/client/modules/voel-audio/android/src/main/java/expo/modules/voel/audio/AudioRecords.kt
+++ apps/client/modules/voel-audio/android/src/main/java/expo/modules/voel/audio/AudioRecords.kt
@@ -6,12 +6,13 @@ import expo.modules.kotlin.records.Record
 class AudioSource(
         @Field val instanceId: String?,
         @Field val bookId: Long,
-        @Field val fileId: Long,
         @Field val chapterId: Long,
         @Field val bookTitle: String,
         @Field val chapterTitle: String,
         @Field val author: String,
-        @Field val fileUri: String,
+        @Field val fileIds: LongArray,
+        @Field val fileUris: ArrayList<String>,
+        @Field val fileDurations: LongArray,
         @Field val artworkUri: String?,
         @Field val startTimeMs: Long,
         @Field val endTimeMs: Long?
