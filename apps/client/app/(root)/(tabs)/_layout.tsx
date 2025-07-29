bfe6ef7e5984b29282223ba858e6a40d724d31f6
Akaanksh Raj <18450543+goknsh@users.noreply.github.com>
2025-07-29 11:56:12 -0500

    feat(client): use native bottom tabs

--- apps/client/app/(root)/(tabs)/_layout.tsx
+++ apps/client/app/(root)/(tabs)/_layout.tsx
@@ -1,37 +1,53 @@
-import { Tabs } from 'expo-router';
+import {
+  type NativeBottomTabNavigationEventMap,
+  type NativeBottomTabNavigationOptions,
+  createNativeBottomTabNavigator,
+} from '@bottom-tabs/react-navigation';
+import type { ParamListBase, TabNavigationState } from '@react-navigation/native';
+import { withLayoutContext } from 'expo-router';
+import { useUnstableNativeVariable } from 'nativewind';
 import { View } from 'react-native';
 
 import { FloatingPlayer, OTAUpdateNotification } from '~/components/floating-player';
-import { Home } from '~/components/icons/Home';
-import { Library } from '~/components/icons/Library';
-import { Settings } from '~/components/icons/Settings';
+
+const BottomTabNavigator = createNativeBottomTabNavigator().Navigator;
+
+const Tabs = withLayoutContext<
+  NativeBottomTabNavigationOptions,
+  typeof BottomTabNavigator,
+  TabNavigationState<ParamListBase>,
+  NativeBottomTabNavigationEventMap
+>(BottomTabNavigator);
 
 export default function TabLayout() {
   return (
     <>
-      <Tabs initialRouteName="(home)" screenOptions={{ headerShown: false }}>
+      <Tabs
+        initialRouteName="(home)"
+        tabBarStyle={{ backgroundColor: `hsl(${useUnstableNativeVariable('--background')})` }}
+        tabLabelStyle={{ fontFamily: 'Voel-Inter-Medium' }}
+        tabBarActiveTintColor={`hsl(${useUnstableNativeVariable('--foreground')})`}
+        activeIndicatorColor={`hsl(${useUnstableNativeVariable('--muted')})`}
+        disablePageAnimations={true}>
         <Tabs.Screen
           name="(library)"
           options={{
             title: 'Library',
-            tabBarIcon: ({ color, size }) => <Library color={color} size={size} />,
-            tabBarLabelStyle: { fontFamily: 'Voel-Inter-Regular' },
+            tabBarIcon: () => require('lucide-static/icons/library.svg'),
           }}
         />
         <Tabs.Screen
           name="(home)"
           options={{
             title: 'Home',
-            tabBarIcon: ({ color, size }) => <Home color={color} size={size} />,
-            tabBarLabelStyle: { fontFamily: 'Voel-Inter-Regular' },
+            tabBarIcon: () => require('lucide-static/icons/house.svg'),
           }}
         />
         <Tabs.Screen
           name="settings"
           options={{
             title: 'Settings',
-            tabBarIcon: ({ color, size }) => <Settings color={color} size={size} />,
-            tabBarLabelStyle: { fontFamily: 'Voel-Inter-Regular' },
+            tabBarIcon: () => require('lucide-static/icons/settings.svg'),
           }}
         />
       </Tabs>
