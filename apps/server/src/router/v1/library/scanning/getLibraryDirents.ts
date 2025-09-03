88cc7f574abec9f0983ce731ea6f0e613a9e3e33
Akaanksh Raj <18450543+goknsh@users.noreply.github.com>
2025-09-02 23:15:25 -0500

    chore(server): more specific error for iterator error

--- apps/server/src/router/v1/library/scanning/getLibraryDirents.ts
+++ apps/server/src/router/v1/library/scanning/getLibraryDirents.ts
@@ -1,23 +1,30 @@
-import { Chunk, Effect, Stream } from 'effect';
+import { Chunk, Data, Effect, Stream } from 'effect';
+
+class DirectoryIteratorError extends Data.TaggedError('DirectoryIteratorError')<{
+  error: string;
+}> {}
 
 export const getLibraryDirents = Effect.fn(function* <
   T extends { name: string; parentPath: string },
 >(dir: AsyncIterable<T>) {
-  const ignoreDirents = new Set<string>();
-  return yield* Stream.fromAsyncIterable(dir, (err) => new Error(String(err))).pipe(
+  const ignoredDirs = new Set<string>();
+  return yield* Stream.fromAsyncIterable(
+    dir,
+    (err) => new DirectoryIteratorError({ error: String(err) })
+  ).pipe(
     Stream.map((entry) => {
       if (entry.name === '.nomedia' || entry.name === '.voelignore') {
-        ignoreDirents.add(entry.parentPath);
+        ignoredDirs.add(entry.parentPath);
       }
       return entry;
     }),
     Stream.runCollect,
-    Effect.catchAll((error) =>
+    Effect.catchTag('DirectoryIteratorError', (error) =>
       Effect.logError('Error while getting library directory entries').pipe(
-        Effect.annotateLogs('error', error.message),
+        Effect.annotateLogs('error', error.error),
         Effect.as(Chunk.empty<T>())
       )
     ),
-    Effect.map((e) => Chunk.filter(e, (dirent) => !ignoreDirents.has(dirent.parentPath)))
+    Effect.map((e) => Chunk.filter(e, (dirent) => !ignoredDirs.has(dirent.parentPath)))
   );
 });
