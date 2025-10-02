d0eb73c558869afe2cbe699b3ed41dbc45c96dc1
Akaanksh Raj <18450543+goknsh@users.noreply.github.com>
2025-10-02 01:11:19 -0500

    chore(client): make ios variant buildable (#1022)

---
+++ apps/client/op-sqlite-fixes.plugin.js
@@ -0,0 +1,18 @@
+/** @import { ExpoConfig } from 'expo/config'; **/
+/** @import { ConfigPlugin } from 'expo/config-plugins'; **/
+const withPodfileProperties = require('expo/config-plugins').withPodfileProperties;
+
+/**
+ * @param {ExpoConfig} config
+ * @returns {ConfigPlugin}
+ */
+const withUseThirdPartySQLitePod = (config) =>
+  withPodfileProperties(config, async (config) => {
+    config.modResults = {
+      ...config.modResults,
+      'expo.updates.useThirdPartySQLitePod': true,
+    };
+    return config;
+  });
+
+module.exports = withUseThirdPartySQLitePod;
