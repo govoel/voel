f996366f12b7b3a20dab899761dd287d230135f5
Akaanksh Raj <18450543+goknsh@users.noreply.github.com>
2025-10-03 16:44:57 -0500

    chore(client): revert to react-native-bottom-tabs (#1026)
    Safe area can’t be relied on for expo-router’s native tabs, content displays under the tab with no way to calculate the tab bar’s height and offset it

--- apps/client/app.config.ts
+++ apps/client/app.config.ts
@@ -18,6 +18,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
     'expo-secure-store',
     'expo-web-browser',
     'expo-localization',
+    'react-native-bottom-tabs',
     [
       'expo-font',
       {
