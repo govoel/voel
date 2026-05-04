4c92c137c21183fc35e814ef16e9459737f62d2b
Akaanksh Raj <18450543+goknsh@users.noreply.github.com>
2026-05-03 22:10:45 -0500

    Squashed '.repos/effect/' changes from f862e4057..71f7c3df9
    71f7c3df9 Port Effect.firstSuccessOf from effect v3 (#2120)
    ef0820365 Version Packages (beta) (#2109)
    7909c954b Fix formatJson circular handling and remove duplicated stringifyCircular (#2119)
    dfbf11114 Normalize SchemaTransformation decoding error messages (#2118)
    f69d5675d add Rpc.custom (#2115)
    7af220790 Add `Schema.DurationFromString`, closes #2092 (#2117)
    848b40a4b Add a `Config.literals` convenience constructor for `Schema.Literals`, closes #2091 (#2116)
    
    git-subtree-dir: .repos/effect
    git-subtree-split: 71f7c3df997deda92c84146d569696dab3bd645c

--- apps/server/src/index.test.ts
+++
@@ -1,5 +0,0 @@
-import { test } from 'bun:test';
-
-test.todo('should start the server and respond to requests', () => {
-  // This test will be implemented in the future to ensure that the server starts correctly and can handle requests.
-});
--- apps/server/src/index.ts
+++
@@ -1,5 +0,0 @@
-import { Effect } from 'effect';
-
-const program = Effect.log('Hello world');
-
-await Effect.runPromise(program);
