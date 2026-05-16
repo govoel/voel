c4d08812000f49455871a805e57713a1390db6cf
Akaanksh Raj <18450543+goknsh@users.noreply.github.com>
2026-05-15 22:38:24 -0500

    save

---
+++ apps/client/src/app/_layout.tsx
@@ -0,0 +1,18 @@
+import { DarkTheme, DefaultTheme, Stack, ThemeProvider } from 'expo-router';
+import { ActivityIndicator, View, useColorScheme } from 'react-native';
+
+export const SuspenseFallback = () => (
+  <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+    <ActivityIndicator size="large" />
+  </View>
+);
+
+export default function TabLayout() {
+  const colorScheme = useColorScheme();
+
+  return (
+    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+      <Stack />
+    </ThemeProvider>
+  );
+}
