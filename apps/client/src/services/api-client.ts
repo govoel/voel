77c85e7aea2a1cb9008b75f9daff8327dc344884
Akaanksh Raj <18450543+goknsh@users.noreply.github.com>
2026-05-16 02:45:18 -0500

    as atomrpc.service

--- apps/client/src/services/api-client.ts
+++ apps/client/src/services/api-client.ts
@@ -1,8 +1,7 @@
-import { Context, Layer } from 'effect';
+import { Layer, Option } from 'effect';
 import { Headers as EffectHeaders, FetchHttpClient } from 'effect/unstable/http';
+import { Atom, AtomRpc } from 'effect/unstable/reactivity';
 import { RpcClient, RpcMiddleware, RpcSerialization } from 'effect/unstable/rpc';
-import type { RpcGroup } from 'effect/unstable/rpc';
-import type { RpcClientError } from 'effect/unstable/rpc/RpcClientError';
 
 import { Api } from '@repo/spec-api';
 import { AuthMiddleware } from '@repo/spec-api/middlewares/auth.ts';
@@ -11,32 +10,41 @@ import type { VoelAuthClient } from '#src/services/auth-client.ts';
 import { encodeServerUrl } from '#src/services/server-url.ts';
 import type { ServerUrl } from '#src/services/server-url.ts';
 
-export class ApiClient extends Context.Service<
-  ApiClient,
-  RpcClient.RpcClient<RpcGroup.Rpcs<typeof Api>, RpcClientError>
->()('voel/services/api-client/ApiClient', {
-  make: RpcClient.make(Api),
-}) {
-  public static readonly layer = ({
-    authClient,
-    serverUrl,
-  }: {
-    readonly authClient: VoelAuthClient;
-    readonly serverUrl: ServerUrl;
-  }) =>
-    Layer.effect(this, this.make).pipe(
-      Layer.provide(
-        RpcMiddleware.layerClient(AuthMiddleware, ({ next, request }) => {
-          const cookie = authClient.getCookie();
+export interface ApiClientConfig {
+  readonly authClient: VoelAuthClient;
+  readonly serverUrl: ServerUrl;
+}
 
-          return next({
-            ...request,
-            headers: EffectHeaders.merge(request.headers, EffectHeaders.fromInput({ cookie })),
-          });
-        })
-      ),
-      Layer.provide(RpcClient.layerProtocolHttp({ url: `${encodeServerUrl(serverUrl)}/api/rpc` })),
+export const ApiClientConfigAtom = Atom.make<Option.Option<ApiClientConfig>>(Option.none()).pipe(
+  Atom.keepAlive
+);
+
+const protocolLayer = ({ authClient, serverUrl }: ApiClientConfig) =>
+  Layer.mergeAll(
+    RpcMiddleware.layerClient(AuthMiddleware, ({ next, request }) => {
+      const cookie = authClient.getCookie();
+
+      return next({
+        ...request,
+        headers: EffectHeaders.merge(request.headers, EffectHeaders.fromInput({ cookie })),
+      });
+    }),
+    RpcClient.layerProtocolHttp({ url: `${encodeServerUrl(serverUrl)}/api/rpc` }).pipe(
       Layer.provide(FetchHttpClient.layer),
       Layer.provide(RpcSerialization.layerMsgPack)
-    );
+    )
+  );
+
+export class ApiClient extends AtomRpc.Service<ApiClient>()('voel/services/api-client/ApiClient', {
+  group: Api,
+  protocol: (get) =>
+    protocolLayer(
+      Option.getOrThrowWith(
+        get(ApiClientConfigAtom),
+        () => new Error('ApiClientConfigAtom must be configured before using ApiClient')
+      )
+    ),
+}) {
+  public static readonly initialConfig = (config: ApiClientConfig) =>
+    Atom.initialValue(ApiClientConfigAtom, Option.some(config));
 }
