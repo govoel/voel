db32daca0240101788e49df2889413049f89b90a
Akaanksh Raj <18450543+goknsh@users.noreply.github.com>
2026-05-22 21:30:29 -0500

    no id

--- apps/client/src/services/database/migrations/000001-base-tables.ts
+++ apps/client/src/services/database/migrations/000001-base-tables.ts
@@ -18,8 +18,8 @@ export const baseTables = Effect.gen(function* () {
   yield* sql`create unique index account_active_uniqueidx on account (active) where active = 1;`;
 
   yield* sql`
-    create trigger account_updatedAt_trigger before update of "serverUrl", username, active on account for each row begin
-      update account set updatedAt = (unixepoch()) where id = new.id;
+    create trigger account_updatedAt_trigger before update of serverUrl, username, active on account for each row begin
+      update account set updatedAt = (unixepoch()) where serverUrl = new.serverUrl and username = new.username;
     end;
   `;
 });
