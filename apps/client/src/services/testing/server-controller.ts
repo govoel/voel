2e9743663e7afe963131495f38071382790a3424
Akaanksh Raj <18450543+goknsh@users.noreply.github.com>
2026-06-30 20:41:13 -0500

    test server layer

---
+++ apps/client/src/services/testing/server-controller.ts
@@ -0,0 +1,77 @@
+import { BunHttpServer, BunRuntime } from '@effect/platform-bun';
+import { Context, Effect, Layer, LayerMap, PlatformError, Schema } from 'effect';
+import { HttpRouter } from 'effect/unstable/http';
+import { ChildProcessSpawner, ChildProcess } from 'effect/unstable/process';
+import { Rpc, RpcGroup, RpcSerialization, RpcServer } from 'effect/unstable/rpc';
+
+const TestServerControllerApi = RpcGroup.make(
+  Rpc.make('start', {
+    payload: Schema.Struct({ port: Schema.Number }),
+    success: Schema.Void,
+    error: Schema.instanceOf(PlatformError.PlatformError),
+  }),
+  Rpc.make('stop', {
+    payload: Schema.Struct({ port: Schema.Number }),
+    success: Schema.Void,
+    error: Schema.instanceOf(PlatformError.PlatformError),
+  })
+);
+
+class RunningTestServer extends Context.Service<RunningTestServer>()(
+  'voel/services/testing/server-controller/RunningTestServer',
+  {
+    make: Effect.fnUntraced(function* ({ port }: { port: number }) {
+      const spawner = yield* ChildProcessSpawner.ChildProcessSpawner;
+
+      const handle = yield* Effect.acquireRelease(
+        spawner.spawn(
+          ChildProcess.make('bun', ['run', 'src/index.ts'], {
+            cwd: '../../apps/server',
+            env: { PORT: port.toString() },
+          })
+        ),
+        (h) => h.kill().pipe(Effect.catch(() => Effect.void))
+      );
+
+      return { handle, port };
+    }),
+  }
+) {
+  public static readonly layer = (args: Parameters<typeof this.make>[0]) =>
+    Layer.effect(this, this.make(args));
+}
+
+class TestServers extends LayerMap.Service<TestServers>()(
+  'voel/services/testing/server-controller/TestServers',
+  {
+    lookup: ({ port }: { port: number }) => RunningTestServer.layer({ port }),
+    idleTimeToLive: 'Infinity',
+  }
+) {}
+
+const TestServerControllerHandlers = Layer.mergeAll(
+  TestServerControllerApi.toLayerHandler('start', ({ port }) =>
+    TestServers.contextEffect({ port }).pipe(Effect.asVoid)
+  ),
+  TestServerControllerApi.toLayerHandler('stop', ({ port }) => TestServers.invalidate({ port }))
+);
+
+const TestServerControllerRoutes = RpcServer.layerHttp({
+  group: TestServerControllerApi,
+  path: '/api/rpc',
+  protocol: 'http',
+  concurrency: 'unbounded',
+}).pipe(
+  Layer.provideMerge(Layer.mergeAll(TestServerControllerHandlers)),
+  Layer.provideMerge(Layer.mergeAll(TestServers.layer, RpcSerialization.layerJson))
+);
+
+if (import.meta.main) {
+  const HttpServerLive = BunHttpServer.layer({ port: 3000 });
+
+  const ServerLive = HttpRouter.serve(TestServerControllerRoutes).pipe(
+    Layer.provide(HttpServerLive)
+  );
+
+  BunRuntime.runMain(Layer.launch(ServerLive));
+}
