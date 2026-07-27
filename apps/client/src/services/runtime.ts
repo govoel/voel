66960bb08382045a90495428b3ffd94f6d701c6d
Akaanksh Raj <18450543+goknsh@users.noreply.github.com>
2026-07-27 18:36:25 -0500

    clean

--- apps/client/src/services/runtime.ts
+++ apps/client/src/services/runtime.ts
@@ -1,7 +1,5 @@
-import { ManagedRuntime } from 'effect';
 import { Atom } from 'effect/unstable/reactivity';
 
 import { CommonClientLayers } from '#src/services/layers.ts';
 
-export const Runtime = ManagedRuntime.make(CommonClientLayers, { memoMap: Atom.defaultMemoMap });
 export const AppRuntime = Atom.runtime(CommonClientLayers);
