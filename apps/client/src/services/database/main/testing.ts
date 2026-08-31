3fded8dd03086640dc9221015398c3cf33e8db8f
Akaanksh Raj <18450543+goknsh@users.noreply.github.com>
2026-08-30 21:29:02 -0500

    test: use Turso sync client in client

--- apps/client/src/services/database/main/testing.ts
+++ apps/client/src/services/database/main/testing.ts
@@ -1,7 +1,7 @@
 import { Context, Effect, Layer } from 'effect';
 import { SqlClient } from 'effect/unstable/sql';
 
-import { TursoClient } from '@repo/effect-turso';
+import { TursoSyncClient } from '@repo/effect-turso-sync';
 
 import { AppConfig } from '#src/services/config.ts';
 import { MainDatabase } from '#src/services/database/main/index.ts';
@@ -11,12 +11,11 @@ export const MainDatabaseTestLayer = MainDatabaseMigrations.layer.pipe(
   Layer.provideMerge(
     Effect.gen(function* () {
       const config = yield* AppConfig;
-      const client = yield* TursoClient.make({
-        filename: config.mainDb.filename,
+      const client = yield* TursoSyncClient.make({
+        path: config.mainDb.filename,
         onConnect: ({ exec }) => exec('PRAGMA foreign_keys = ON'),
       });
 
-      // @ts-expect-error -- @repo/effect-turso and @repo/effect-turso-rn have different config types, but they share SqlClient.SqlClient so this is fine
       return Context.make(MainDatabase, client).pipe(Context.add(SqlClient.SqlClient, client));
     }).pipe(Layer.effectContext)
   )
